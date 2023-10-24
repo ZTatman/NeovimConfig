a8f1593ac0dfcbd419ee4ea1398226083c606c44
ZTatman <zachariah.tatman@charter.com>
2023-10-24 16:42:46 -0400

    update: add pair rules for markdown and tex files

--- lua/core/plugin_config/autopairs.lua
+++ lua/core/plugin_config/autopairs.lua
@@ -1,6 +1,9 @@
 local status, autopairs = pcall(require, "nvim-autopairs")
 if (not status) then return end
 
+local Rule = require('nvim-autopairs.rule')
+local npairs = require('nvim-autopairs')
+
 autopairs.setup({
     disable_filetype = { "TelescopePrompt", "vim" },
     enable_check_bracket_line = false, --if next character is a close pair and it doesn't have an open pair in same line, then it will not add a close pair
@@ -12,3 +15,7 @@ autopairs.setup({
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
 cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+
+npairs.add_rule(Rule("$","$",{"tex", "markdown"}))
+npairs.add_rule(Rule("**","**", "markdown"))
+npairs.add_rule(Rule("_","_", "markdown"))
