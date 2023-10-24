a5893fe5489979fe3e0e57f5ba84d72029efeef4
ZTatman <zachariah.tatman@charter.com>
2023-10-24 16:45:22 -0400

    update: added RainbowRed hl group and use it as color for all hl scopes

--- lua/core/plugin_config/indent-blankline.lua
+++ lua/core/plugin_config/indent-blankline.lua
@@ -5,13 +5,7 @@ vim.opt.list = true
 vim.opt.listchars:append "space: "
 vim.opt.listchars:append "eol: "
 
--- vim.opt.termguicolors = true
--- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C586C0 gui=nocombine]]
+vim.opt.termguicolors = true
 
 vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
 vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
@@ -23,7 +17,7 @@ vim.api.nvim_set_hl(0, "RainbowPurple", { fg = "#C586C0" })
 indent.setup({
     scope = {
         highlight = {
-            "RainbowRed",
+            "RainbowRed"
         },
     },
     exclude = {
@@ -32,7 +26,9 @@ indent.setup({
             "NvimTree",
             "dashboard",
             "markdown",
-            "markdown-inline"
+            "markdown-inline",
+            "telescope",
+            "telescope_prompt"
         }
     }
 })
