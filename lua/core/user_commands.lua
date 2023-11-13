b6359d02d8ee39b5c0f1a4bcd5b898626f53e29e
ZTatman <zachariah.tatman@charter.com>
2023-11-13 15:32:29 -0500

    update: add a user command to watch current buffer and refresh after changes are made

--- lua/core/user_commands.lua
+++ lua/core/user_commands.lua
@@ -1,8 +1,15 @@
+local o, api = vim.o, vim.api
 local fn = require("core.util.functions")
 local create_cmd = require("core.util.utils").create_cmd
 
 create_cmd("UpdateAndSyncAll", fn.update_all)
-
 create_cmd("Help", fn.help_select)
 create_cmd("HelpWord", fn.help_word)
 create_cmd("HelpGrep", fn.help_grep)
+
+o.autoread = true
+api.nvim_create_autocmd(
+    { "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+        command = "if mode() != 'c' | checktime | endif",
+        pattern = { "*" },
+    })
