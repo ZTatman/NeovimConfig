5925675ee6a2abfab98970f299cb885ffe623a13
ZTatman <zachariah.tatman@charter.com>
2023-12-06 09:56:41 -0500

    update: simplify bufferline.lua config

--- lua/core/plugin_config/bufferline.lua
+++ lua/core/plugin_config/bufferline.lua
@@ -6,20 +6,9 @@ bufferline.setup {
     options = {
         mode = "tabs",
         numbers = "none",
-        number_style = "",
-        mappings = true,
-        -- indicator = {
-        --     -- icon = '▎', -- this should be omitted if indicator style is not 'icon'
-        --     style = 'none',
-        -- },
-        buffer_close_icon = "x",
         modified_icon = "●",
-        close_icon = "",
         left_trunc_marker = "",
         right_trunc_marker = "",
-        max_name_length = 12,
-        max_prefix_length = 10,
-        tab_size = 20,
         padding = { left = 4, right = 4 },
         diagnostics = "nvim_lsp",
         diagnostics_indicator = function(count, level)
@@ -35,15 +24,15 @@ bufferline.setup {
             }
         },
         color_icons = true,
-        show_buffer_icons = true,
-        show_buffer_close_icons = true,
-        show_close_icon = true,
+        -- show_buffer_icons = true,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
         show_tab_indicators = true,
-        persist_buffer_sort = true,
+        -- persist_buffer_sort = true,
         separator_style = "thick",
         enforce_regular_tabs = true,
         always_show_bufferline = true,
-        sort_by = "id",
+        sort_by = "tabs",
         custom_areas = {
             left = function()
                 local result = {}
