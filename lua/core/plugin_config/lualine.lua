94b39d78ed302c9c77becef9f41fabcb41548026
ZTatman <zachariah.tatman@charter.com>
2023-12-06 09:57:51 -0500

    update: change lualine colorscheme

--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -16,7 +16,7 @@ end
 lualine.setup {
     options = {
         icons_enabled = true,
-        theme = 'edge',
+        theme = 'kanagawa',
         section_separators = { left = '', right = '' },
         component_separators = { left = '|', right = '|' },
         disabled_filetypes = { 'alpha', 'NvimTree', 'Outline' },
