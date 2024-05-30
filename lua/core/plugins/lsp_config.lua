b2e24fe986dc7ee67a5c47f0c8008187fad144c4
ZTatman <zachariah.tatman@charter.com>
2024-05-30 15:25:59 -0400

    fix: add eslint experimental.flatConfig option to fix diagnostics

--- lua/core/plugins/lsp_config.lua
+++ lua/core/plugins/lsp_config.lua
@@ -5,31 +5,30 @@ local lspconfig = require("lspconfig")
 
 -- Quickfix code_action
 function quickfix()
-    vim.lsp.buf.code_action({
-        filter = function(a)
-            return a.isPreferred
-        end,
-        apply = true,
-    })
-
+	vim.lsp.buf.code_action({
+		filter = function(a)
+			return a.isPreferred
+		end,
+		apply = true,
+	})
 end
 
 function get_eslint_override_config_file()
-    if fn.getcwd():match("Charter") and os.getenv("ESLINT_CHARTER_CONFIG") then
-        print("Eslint: using charter gateway eslint rules....")
-        return os.getenv("ESLINT_CHARTER_CONFIG")
-    else
-        return nil
-    end
+	if fn.getcwd():match("Charter") and os.getenv("ESLINT_CHARTER_CONFIG") then
+		print("Eslint: using charter gateway eslint rules....")
+		return os.getenv("ESLINT_CHARTER_CONFIG")
+	else
+		return nil
+	end
 end
 
 function on_attach(client, bufnr)
-    if client.name == "tailwindcss" then
-        client.server_capabilities.documentFormattingProvider = false
-    else
-        client.server_capabilities.documentFormattingProvider = true
-    end
-    k.map_lsp_keys(bufnr)
+	if client.name == "tailwindcss" then
+		client.server_capabilities.documentFormattingProvider = false
+	else
+		client.server_capabilities.documentFormattingProvider = true
+	end
+	k.map_lsp_keys(bufnr)
 end
 
 local opts = { noremap = true, silent = true }
@@ -41,128 +40,131 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities(client_capabil
 
 -- Eslint
 lspconfig.eslint.setup({
-    on_attach = on_attach,
-    filetypes = { "javascript", "javascriptreact", "javascript.jsx", "vue" },
-    capabilities = capabilities,
-    settings = {
-        nodePath = os.getenv("ESLINT_PATH"),
-        options = {
-            overrideConfigFile = get_eslint_override_config_file(),
-        },
-    },
+	on_attach = on_attach,
+	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "vue" },
+	capabilities = capabilities,
+	settings = {
+		experimental = {
+			useFlatConfig = false,
+		},
+		nodePath = os.getenv("ESLINT_PATH"),
+		options = {
+			overrideConfigFile = get_eslint_override_config_file(),
+		},
+	},
 })
 
 -- Typescript
 lspconfig.tsserver.setup({
-    on_attach = on_attach,
-    filetypes = { "javascript", "typescript", "typescript.tsx", "typescriptreact" },
-    capabilities = capabilities,
+	on_attach = on_attach,
+	filetypes = { "javascript", "typescript", "typescript.tsx", "typescriptreact" },
+	capabilities = capabilities,
 })
 
 -- Tailwindcss
 lspconfig.tailwindcss.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
+	on_attach = on_attach,
+	capabilities = capabilities,
 })
 
 -- Vue
 lspconfig.volar.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "typescript", "javascript", "vue" }
+	on_attach = on_attach,
+	capabilities = capabilities,
+	filetypes = { "typescript", "javascript", "vue" },
 })
 
 -- Markdown
 lspconfig.marksman.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
+	on_attach = on_attach,
+	capabilities = capabilities,
 })
 
 -- -- Java
-lspconfig.jdtls.setup {
-    on_attach = on_attach,
-    capabilities = capabilities
-}
+lspconfig.jdtls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
 
 -- HTML
 lspconfig.html.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "html", "vue" }
+	on_attach = on_attach,
+	capabilities = capabilities,
+	filetypes = { "html", "vue" },
 })
 
 -- CSS
 lspconfig.cssls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "css", "scss", "less", "sass" }
+	on_attach = on_attach,
+	capabilities = capabilities,
+	filetypes = { "css", "scss", "less", "sass" },
 })
 
 -- CSS Modules
 lspconfig.cssmodules_ls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "css", "scss", "less", "sass", "vue" }
+	on_attach = on_attach,
+	capabilities = capabilities,
+	filetypes = { "css", "scss", "less", "sass", "vue" },
 })
 
 -- Emmet
 lspconfig.emmet_ls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
+	on_attach = on_attach,
+	capabilities = capabilities,
 })
 
 --  Lua
 lspconfig.lua_ls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    settings = {
-        Lua = {
-            runtime = {
-                version = "LuaJIT",
-            },
-            diagnostics = {
-                globals = { "vim", "require" },
-            },
-            workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
-                checkThirdParty = false,
-            },
-            telemetry = { enable = false },
-            hint = {
-                enable = true,
-            },
-        },
-    },
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = { "vim", "require" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
+			telemetry = { enable = false },
+			hint = {
+				enable = true,
+			},
+		},
+	},
 })
 
 -- Diagnostics
 local signs = {
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignInfo", text = "󰋼" },
-    { name = "DiagnosticSignHint", text = "󱗞" },
+	{ name = "DiagnosticSignError", text = "" },
+	{ name = "DiagnosticSignWarn", text = "" },
+	{ name = "DiagnosticSignInfo", text = "󰋼" },
+	{ name = "DiagnosticSignHint", text = "󱗞" },
 }
 
 for _, sign in ipairs(signs) do
-    fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+	fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
 end
 
 vim.diagnostic.config({
-    -- disable virtual text
-    virtual_text = false,
-    -- show signs
-    signs = {
-        active = signs,
-    },
-    update_in_insert = true,
-    underline = true,
-    severity_sort = true,
-    float = {
-        focusable = false,
-        style = "minimal",
-        border = "rounded",
-        source = "always",
-        header = "",
-        prefix = "",
-    },
+	-- disable virtual text
+	virtual_text = false,
+	-- show signs
+	signs = {
+		active = signs,
+	},
+	update_in_insert = true,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
 })
