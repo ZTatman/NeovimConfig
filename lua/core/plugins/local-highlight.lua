707c1897b318169a3697fce9d7dd2b8f0d2e41c8
ZTatman <zachariah.tatman@charter.com>
2024-05-30 15:25:04 -0400

    chore: remove unused local-highlight plugin

--- lua/core/plugins/local-highlight.lua
+++
@@ -1,6 +0,0 @@
--- require('local-highlight').setup({
---     -- OR attach to every filetype except:
---     disable_file_types = { 'tex' },
---     hlgroup = 'None',
---     cw_hlgroup = 'Underlined',
--- })
